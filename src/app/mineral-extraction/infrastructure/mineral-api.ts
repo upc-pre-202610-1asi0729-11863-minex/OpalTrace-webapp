dfa8d26637d024a6b5e2a4b5bb4dc4cadcb8f62c
santibal11 <santibal.1604@gmail.com>
2026-05-12 22:05:43 -0500

    feat(mineral-extraction): add mineral api

--- src/app/mineral-extraction/infrastructure/mineral-api.ts
+++ src/app/mineral-extraction/infrastructure/mineral-api.ts
@@ -1,1 +1,29 @@
-﻿
+﻿import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { BaseApi } from '../../shared/infrastructure/base-api';
+import { MineralBatch } from '../domain/model/mineral-batch.entity';
+import { AnomalyAlert } from '../domain/model/anomaly-alert.entity';
+import { BatchesApiEndpoint } from './batches-api-endpoint';
+import { AlertsApiEndpoint } from './alerts-api-endpoint';
+
+@Injectable({ providedIn: 'root' })
+export class MineralApi extends BaseApi {
+  private readonly batchesEndpoint: BatchesApiEndpoint;
+  private readonly alertsEndpoint: AlertsApiEndpoint;
+
+  constructor(http: HttpClient) {
+    super();
+    this.batchesEndpoint = new BatchesApiEndpoint(http);
+    this.alertsEndpoint  = new AlertsApiEndpoint(http);
+  }
+
+  getBatchesByUser(userId: number): Observable<MineralBatch[]>        { return this.batchesEndpoint.getByUserId(userId); }
+  getBatch(id: number): Observable<MineralBatch>                     { return this.batchesEndpoint.getById(id); }
+  getBatchByBatchId(batchId: string): Observable<MineralBatch | null>{ return this.batchesEndpoint.getBatchByBatchId(batchId); }
+  createBatch(batch: MineralBatch): Observable<MineralBatch>         { return this.batchesEndpoint.create(batch); }
+  updateBatch(batch: MineralBatch): Observable<MineralBatch>         { return this.batchesEndpoint.update(batch, batch.id); }
+
+  getAlertsByUser(userId: number): Observable<AnomalyAlert[]>  { return this.alertsEndpoint.getByUserId(userId); }
+  createAlert(alert: AnomalyAlert): Observable<AnomalyAlert>   { return this.alertsEndpoint.create(alert); }
+}
