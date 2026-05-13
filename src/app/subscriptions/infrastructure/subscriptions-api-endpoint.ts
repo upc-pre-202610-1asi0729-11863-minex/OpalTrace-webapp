a5cc5fa7444634394f466b1e5c47fe4771fb1345
rose <rosealmendra.calderon@gmail.com>
2026-05-12 21:14:59 -0500

    feat(subscriptions): add subscriptions api

--- src/app/subscriptions/infrastructure/subscriptions-api-endpoint.ts
+++ src/app/subscriptions/infrastructure/subscriptions-api-endpoint.ts
@@ -1,1 +1,21 @@
-﻿
+﻿import { HttpClient } from '@angular/common/http';
+import { BaseApiEndpoint } from '../../shared/infrastructure/base-api-endpoint';
+import { Subscription } from '../domain/model/subscription.entity';
+import { SubscriptionResource, SubscriptionsResponse } from './subscription.resource';
+import { SubscriptionAssembler } from './subscription.assembler';
+import { environment } from '../../../environments/environment';
+
+export class SubscriptionsApiEndpoint extends BaseApiEndpoint<
+  Subscription,
+  SubscriptionResource,
+  SubscriptionsResponse,
+  SubscriptionAssembler
+> {
+  constructor(http: HttpClient) {
+    super(
+      http,
+      `${environment.platformProviderApiBaseUrl}${environment.platformProviderSubscriptionsEndpointPath}`,
+      new SubscriptionAssembler()
+    );
+  }
+}
