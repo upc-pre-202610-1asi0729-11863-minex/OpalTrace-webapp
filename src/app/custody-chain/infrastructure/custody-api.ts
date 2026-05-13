339d48e71bedd234124761ab7e787380c5bcba69
Adrian5102 <u202410084@upc.edu.pe>
2026-05-12 21:55:18 -0500

    feat(custody-chain): add custody api

--- src/app/custody-chain/infrastructure/custody-api.ts
+++ src/app/custody-chain/infrastructure/custody-api.ts
@@ -1,1 +1,24 @@
-﻿
+﻿import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { BaseApi } from '../../shared/infrastructure/base-api';
+import { LocationUpdateRecord } from '../domain/model/location-update.entity';
+import { LocationUpdatesApiEndpoint } from './location-updates-api-endpoint';
+
+@Injectable({ providedIn: 'root' })
+export class CustodyApi extends BaseApi {
+  private readonly locationUpdatesEndpoint: LocationUpdatesApiEndpoint;
+
+  constructor(http: HttpClient) {
+    super();
+    this.locationUpdatesEndpoint = new LocationUpdatesApiEndpoint(http);
+  }
+
+  getLocationUpdates(): Observable<LocationUpdateRecord[]> {
+    return this.locationUpdatesEndpoint.getAll();
+  }
+
+  createLocationUpdate(record: LocationUpdateRecord): Observable<LocationUpdateRecord> {
+    return this.locationUpdatesEndpoint.create(record);
+  }
+}
