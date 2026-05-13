95c93ec5c442e6f8c4baa3ba62c9489974e89e16
rose <rosealmendra.calderon@gmail.com>
2026-05-12 21:13:02 -0500

    feat(subscriptions): add billing record assembler

--- src/app/subscriptions/infrastructure/billing-record.assembler.ts
+++ src/app/subscriptions/infrastructure/billing-record.assembler.ts
@@ -1,1 +1,36 @@
-﻿
+﻿import { BaseAssembler } from '../../shared/infrastructure/base-assembler';
+import { BillingRecord } from '../domain/model/billing-record.entity';
+import { BillingRecordResource, BillingRecordsResponse } from './billing-record.resource';
+
+export class BillingRecordAssembler
+  implements BaseAssembler<BillingRecord, BillingRecordResource, BillingRecordsResponse> {
+
+  toEntityFromResource(resource: BillingRecordResource): BillingRecord {
+    return new BillingRecord({
+      id: resource.id,
+      userId: resource.userId,
+      date: resource.date,
+      plan: resource.plan,
+      amount: resource.amount,
+      status: resource.status,
+    });
+  }
+
+  toResourceFromEntity(entity: BillingRecord): BillingRecordResource {
+    return {
+      id: entity.id,
+      userId: entity.userId,
+      date: entity.date,
+      plan: entity.plan,
+      amount: entity.amount,
+      status: entity.status,
+    };
+  }
+
+  toEntitiesFromResponse(response: BillingRecordsResponse): BillingRecord[] {
+    if (response.billingRecords) {
+      return response.billingRecords.map(r => this.toEntityFromResource(r));
+    }
+    return [];
+  }
+}
