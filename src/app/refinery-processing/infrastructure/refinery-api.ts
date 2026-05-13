d491803289a84eceb5e1b930f0285f6c14e51023
Ethan Yi <mryiethan@gmail.com>
2026-05-12 22:30:45 -0500

    feat(refinery-processing): add refinery api

--- src/app/refinery-processing/infrastructure/refinery-api.ts
+++ src/app/refinery-processing/infrastructure/refinery-api.ts
@@ -1,1 +1,56 @@
-﻿
+﻿import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { BaseApi } from '../../shared/infrastructure/base-api';
+import { RefineryBatch } from '../domain/model/refinery-batch.entity';
+import { SubLot } from '../domain/model/sublot.entity';
+import { ShrinkageRecord } from '../domain/model/shrinkage-record.entity';
+import { RefineryBatchesApiEndpoint } from './refinery-batches-api-endpoint';
+import { SublotsApiEndpoint } from './sublots-api-endpoint';
+import { ShrinkageRecordsApiEndpoint } from './shrinkage-records-api-endpoint';
+
+@Injectable({ providedIn: 'root' })
+export class RefineryApi extends BaseApi {
+  private readonly refineryBatchesEndpoint: RefineryBatchesApiEndpoint;
+  private readonly sublotsEndpoint: SublotsApiEndpoint;
+  private readonly shrinkageRecordsEndpoint: ShrinkageRecordsApiEndpoint;
+
+  constructor(http: HttpClient) {
+    super();
+    this.refineryBatchesEndpoint = new RefineryBatchesApiEndpoint(http);
+    this.sublotsEndpoint = new SublotsApiEndpoint(http);
+    this.shrinkageRecordsEndpoint = new ShrinkageRecordsApiEndpoint(http);
+  }
+
+  getRefineryBatches(): Observable<RefineryBatch[]> {
+    return this.refineryBatchesEndpoint.getAll();
+  }
+
+  createRefineryBatch(batch: RefineryBatch): Observable<RefineryBatch> {
+    return this.refineryBatchesEndpoint.create(batch);
+  }
+
+  updateRefineryBatch(batch: RefineryBatch): Observable<RefineryBatch> {
+    return this.refineryBatchesEndpoint.update(batch, batch.id);
+  }
+
+  getSublots(): Observable<SubLot[]> {
+    return this.sublotsEndpoint.getAll();
+  }
+
+  createSublot(sublot: SubLot): Observable<SubLot> {
+    return this.sublotsEndpoint.create(sublot);
+  }
+
+  updateSublot(sublot: SubLot): Observable<SubLot> {
+    return this.sublotsEndpoint.update(sublot, sublot.id);
+  }
+
+  getShrinkageRecords(): Observable<ShrinkageRecord[]> {
+    return this.shrinkageRecordsEndpoint.getAll();
+  }
+
+  createShrinkageRecord(record: ShrinkageRecord): Observable<ShrinkageRecord> {
+    return this.shrinkageRecordsEndpoint.create(record);
+  }
+}
