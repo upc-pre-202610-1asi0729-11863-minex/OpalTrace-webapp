764e4cbcf7cfb3fdaf340e702c4b7e496d65e001
rose <rosealmendra.calderon@gmail.com>
2026-05-12 21:14:45 -0500

    feat(subscriptions): add subscriptions api endpoint

--- src/app/subscriptions/infrastructure/subscriptions-api.ts
+++ src/app/subscriptions/infrastructure/subscriptions-api.ts
@@ -1,1 +1,40 @@
-﻿
+﻿import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { BaseApi } from '../../shared/infrastructure/base-api';
+import { Subscription } from '../domain/model/subscription.entity';
+import { BillingRecord } from '../domain/model/billing-record.entity';
+import { SubscriptionsApiEndpoint } from './subscriptions-api-endpoint';
+import { BillingRecordsApiEndpoint } from './billing-records-api-endpoint';
+
+@Injectable({ providedIn: 'root' })
+export class SubscriptionsApi extends BaseApi {
+  private readonly subscriptionsEndpoint: SubscriptionsApiEndpoint;
+  private readonly billingRecordsEndpoint: BillingRecordsApiEndpoint;
+
+  constructor(private http: HttpClient) {
+    super();
+    this.subscriptionsEndpoint = new SubscriptionsApiEndpoint(http);
+    this.billingRecordsEndpoint = new BillingRecordsApiEndpoint(http);
+  }
+
+  getSubscriptions(): Observable<Subscription[]> {
+    return this.subscriptionsEndpoint.getAll();
+  }
+
+  getSubscription(id: number): Observable<Subscription> {
+    return this.subscriptionsEndpoint.getById(id);
+  }
+
+  updateSubscription(subscription: Subscription): Observable<Subscription> {
+    return this.subscriptionsEndpoint.update(subscription, subscription.id);
+  }
+
+  getBillingRecords(): Observable<BillingRecord[]> {
+    return this.billingRecordsEndpoint.getAll();
+  }
+
+  createBillingRecord(record: BillingRecord): Observable<BillingRecord> {
+    return this.billingRecordsEndpoint.create(record);
+  }
+}
